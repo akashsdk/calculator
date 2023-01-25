56b2d3ebdd4509025a93dc7c7d68a53b105b4250
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-01-25 19:32:30 +0600

    ok

--- src/Component/header.js
+++ src/Component/header.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import './header.css';
-
+import React from "react";
+import "./header.css";
+import { Button } from "antd";
+import { Link } from "react-router-dom";
 
 export default function header() {
   return (
-    <div className='headerBody'>
-      <div className='headerBody2'>
-        <h1 className='headerTittle'>Calculator</h1>
-        <div className='headerBox'>alalal</div>
+    <div className="headerBody">
+      <div className="headerBody2">
+        <h1 className="headerTittle">Calculator</h1>
+        <div className="headerBox">
+          <Button type="dashed" >
+          <Link  to="/">Home</Link>
+          </Button>
+          <Button type="dashed" style={{marginLeft:'20px'}}>
+            About
+          </Button>
+          <Button type="dashed" style={{marginLeft:'20px'}}>
+            Contact
+          </Button>
+        </div>
+        <div>aaaaa</div>
       </div>
     </div>
-  )
+  );
 }
