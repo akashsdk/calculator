11d646b5524e38e91d54adbab7538eb83ee643f9
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-01-25 17:34:38 +0600

    ok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
-import Home from './Screen/home';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./Screen/home";
+import ErrorPage from "./Component/errorPage";
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Routes>
+        <Route path="*" element={<ErrorPage />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
