a0608730e66cb5967edc7026dbe20f4a8c1cfc2d
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-01-28 19:18:01 +0600

    Update home.js

--- src/Screen/home.js
+++ src/Screen/home.js
@@ -22,18 +22,12 @@ export default function Home() {
       {page == 1 ? (
         <p className="noticeHedertext">Board-1</p>
       ) : page == 2 ? (
-        // Calendar
         <p className="noticeHedertext">Calendar</p>
       ) : page == 3 ? (
-        // Notice Board
         <p className="noticeHedertext">Notice Board</p>
       ) : page == 4 ? (
-        // Events
         <p className="noticeHedertext">Events</p>
-      ) : page == 5 ? (
-        <p className="noticeHedertext">Board-5</p>
-      ) : (
-        // Error Page
+      )  : (
         <p className="noticeHedertext">Error !!</p>
       )}
       </div>
