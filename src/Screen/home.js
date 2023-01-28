8087de8755aa8cc0a2fdec877cea7aa7b549b141
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-01-28 19:17:46 +0600

    Update home.js

--- src/Screen/home.js
+++ src/Screen/home.js
@@ -6,11 +6,37 @@ import { CalculatorOutlined } from "@ant-design/icons";
 
 
 export default function Home() {
+  const [current, setCurrent] = useState(1);
+  const [page, setPage] = useState(1);
+
+  const onChange = (page) => {
+    console.log(page);
+    setCurrent(page);
+    setPage(page);
+  };
   return (
     <div className="homeBody">
       
 
-      <div className="homeLeftBody"></div>
+      <div className="homeLeftBody">
+      {page == 1 ? (
+        <p className="noticeHedertext">Board-1</p>
+      ) : page == 2 ? (
+        // Calendar
+        <p className="noticeHedertext">Calendar</p>
+      ) : page == 3 ? (
+        // Notice Board
+        <p className="noticeHedertext">Notice Board</p>
+      ) : page == 4 ? (
+        // Events
+        <p className="noticeHedertext">Events</p>
+      ) : page == 5 ? (
+        <p className="noticeHedertext">Board-5</p>
+      ) : (
+        // Error Page
+        <p className="noticeHedertext">Error !!</p>
+      )}
+      </div>
       <div className="homeMidelBody"></div>
 
       <div className="homeRightBody">
@@ -18,15 +44,21 @@ export default function Home() {
         <div className="homeLine" />
 
         <div className="homeRightBox">
-          <h2 style={{ opacity: ".6" }}>Calculator</h2>
+          <h2 style={{ color: page == 1 ? 'black' : 'rgb(152, 152, 158)', }}>Calculator</h2>
           <div className="homeRightIconBox">
-            <Button shape="circle" icon={<CalculatorOutlined />} />
+            <Button shape="circle" icon={<CalculatorOutlined />} 
+            onClick={() => {
+              setPage(1);
+            }}/>
           </div>
         </div>
         <div className="homeRightBox">
-          <h2 style={{ opacity: ".6" }}>Calculator</h2>
+          <h2 style={{ color: page == 2 ? 'black' : 'rgb(152, 152, 158)', }}>Calculator</h2>
           <div className="homeRightIconBox">
-            <Button shape="circle" icon={<CalculatorOutlined />} />
+            <Button shape="circle" icon={<CalculatorOutlined />} 
+            onClick={() => {
+              setPage(2);
+            }}/>
           </div>
         </div>
       </div>
