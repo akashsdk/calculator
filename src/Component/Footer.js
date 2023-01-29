b2a38daf30caeed314ed29654d738f55f23b26dd
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-01-30 02:13:30 +0600

    ok

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -3,6 +3,9 @@ import './Footer.css';
 
 export default function Footer() {
   return (
-    <div className='footerBody'>Footer</div>
+    <div className='footerBody'>
+        <div>ssss</div>
+        <p className='footerDownP'>Copyright © 2023 · All Rights Reserved</p>
+    </div>
   )
 }
