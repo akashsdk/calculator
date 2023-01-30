120bbf176aa4e747810b96056e031b6bb7dccf1d
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-01-31 01:24:54 +0600

    Update Footer.js

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css';
 export default function Footer() {
   return (
     <div className='footerBody'>
-        <div>ssss</div>
+        <div>ssss vvv</div>
         <a href='https://tasnim-shahriar-akash.web.app/' className='footerATage'>Developer Info.</a>
         <p className='footerDownP'>Copyright © 2023 · All Rights Reserved</p>
     </div>
